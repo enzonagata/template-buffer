614e7df6944edd115f5d0b636d13bbafa700bcf9
Enzo Nagata <enzo.nagata@gmail.com>
2018-04-18 13:55:24 -0300

    Initial estructure

---
+++ Html.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Nagata;
+
+class Html
+{
+    protected $routes;
+    protected $request;
+    public $styles = [];
+    public $scripts = [];
+
+    public function __construct($container)
+    {
+        $this->routes = $container->get('router');
+        $this->request = $container->get('request');
+        $this->base_url = $this->request->getUri()->getBaseUrl();
+    }
+
+    public function addStyles($uri)
+    {
+        if (is_array($uri)) {
+            foreach ($uri as $uri2) {
+                $this->addStyles($uri2);
+            }
+
+        } elseif (!in_array($uri, $this->styles)) {
+            $this->styles[] = $uri;
+        }
+
+    }
+
+    /* show css */
+    public function css()
+    {
+        $_css_html = '';
+        foreach ($this->styles as $css) {
+            if (preg_match('(http|https)', $css)) {
+                $_css_html .= '<link href = "' . $css . '" rel = "stylesheet" type = "text/css" />';
+            } else {
+                $_css_html .= '<link href = "' . $this->base_url . '/css/' . $css . '.css" rel = "stylesheet" type = "text/css" />';
+            }
+        }
+        echo $_css_html;
+    }
+
+    /* Stylesheet javascript add */
+
+    public function addScripts($uri)
+    {
+        if (is_array($uri)) {
+            foreach ($uri as $uri2) {
+                $this->addScripts($uri2);
+            }
+        } elseif (!in_array($uri, $this->scripts)) {
+            $this->scripts[] = $uri;
+        }
+
+    }
+
+    /* show css */
+
+    public function js()
+    {
+        $_js_html = '';
+        foreach ($this->scripts as $js) {
+            $_js_html .= '<script src = "' . $this->base_url . '/js/' . $js . '.js"></script>';
+        }
+        echo $_js_html;
+    }
+
+     //set template
+     public function pathFor($route_name, $array = [], $args = [])
+     {
+         return $this->routes->pathFor($route_name, $array, $args);
+     }
+
+     public function image($img, $class = "") {
+      return '<img src = "' . $this->base_url . '/images/' . $img . '" class = "' . $class . '"/>';
+  }
+
+  public function confirm_link($id, $url) {
+      return '<form action="' . $url . '" method="POST" onsubmit="return confirm(\'Deseja mesmo excluir o registro?\')" >'
+      . "<input type='hidden' name='id' value='" . $id . "'/>"
+      . "<button class='btn-flat' type='submit'><i class='material-icons'>delete</i></button>"
+      . "</form>";
+  }
+
+
+
+}
---
+++ Template.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Nagata;
+
+class Template
+{
+
+    protected $template_path;
+    protected $layout_path;
+    protected $root_path;
+    protected $layout;
+    protected $template;
+
+    public $Html;
+    public $Text;
+
+    public function __construct($container)
+    {
+        $this->root_path = $container['settings']['app_path'];
+        $this->setLayoutPath($container['settings']['layout_path']);
+        $this->setTemplatePath($container['settings']['template_path']);
+        $this->layout();
+    }
+
+    public function setLayoutPath($path)
+    {
+        $this->layout_path = $path;
+    }
+
+    public function setTemplatePath($path)
+    {
+        $this->template_path = $this->template_path . $path;
+    }
+
+    //set layout
+    public function layout($layout = 'default')
+    {
+        $this->layout = $layout;
+    }
+
+    //set variables
+    public function set($key, $value)
+    {
+        $this->values[$key] = $value;
+    }
+
+    public function render($template)
+    {
+        $templatePath = $this->template_path . ltrim($template . ".phtml", DS);
+        if (!file_exists($templatePath)) {
+            die('Não foi possível encontrar o template ' . $templatePath . '.');
+        }
+        @extract($this->values);
+        ob_start();
+        require $templatePath;
+        $html = ob_get_clean();
+        echo $this->_render_layout($html);
+    }
+
+    public function _render_layout($_content)
+    {
+        @extract($this->values);
+        $layout_path = $this->layout_path . ltrim($this->layout . '.phtml', DS);
+        if ($this->layout !== null) {
+            if (!file_exists($layout_path)) {
+                die('Não foi possível encontrar o layout ' . $templatePath . '.');
+            }
+            ob_start();
+            require $layout_path;
+            $_content = ob_get_clean();
+        }
+        return $_content;
+    }
+
+}
