122d6d6fc55a7004ffe1fd7956008d5c251657b8
Enzo <enzo.nagata@gmail.com>
2018-04-24 17:28:51 -0300

    Fixed function clean()

--- Text.php
+++ Text.php
@@ -8,6 +8,7 @@ namespace Nagata;
 class Text {
     public function clean($string) {
         $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+        $string = str_replace('-', '', $string); // Replaces all spaces with hyphens.
         return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
     }
     public function slugify($text){
